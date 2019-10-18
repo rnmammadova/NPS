f9a50b10419af9dbf34ea8903555728d8b8acf28
m-abdulhak <m_abdulhak@outlook.com>
2019-10-17 21:59:39 -0230

    Change port to 3331

--- server.js
+++ server.js
@@ -48,8 +48,8 @@ app.post('/', function (req, res) {
   });
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(3331, function () {
+  console.log('Example app listening on port 3331!')
 })
 
 
